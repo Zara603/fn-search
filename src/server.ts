0aa8406018d04531955ae1cf1a376a37f14b99c4
colin pringlewood <colin.pringlewood@gmail.com>
2020-06-25 09:55:51 +1000

    re adding auth

--- src/server.ts
+++ src/server.ts
@@ -22,8 +22,7 @@ export default function server(): any {
 
   app.get("/api/search/index-offers", adminMiddleware, scriptController.index);
 
-  //app.get("/api/search/update-sfmc", adminMiddleware, scriptController.sendDataToSFMC);
-  app.get("/api/search/update-sfmc", scriptController.sendDataToSFMC);
+  app.get("/api/search/update-sfmc", adminMiddleware, scriptController.sendDataToSFMC);
 
   // TODO remove this route before going into production.
   app.get("/api/search/flush-all", adminMiddleware, adminController.flushAll);
